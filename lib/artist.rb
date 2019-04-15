06cc7c12a6f63e3bf5557ad4b502c57a61e92718
Dan Hwang <danhwangfkpc@gmail.com>
2019-04-15 20:27:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,12 @@
 class Artist
   attr_accessor :name, :songs
   @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+  end 
